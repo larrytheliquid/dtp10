42da8ed7b2ad5681c0564ab1d5f60a2ac29aee4b
Larry Diehl <larrytheliquid@gmail.com>
2010-07-07 23:02:23 -0700

    Look more functional

--- six.rb
+++ six.rb
@@ -16,16 +16,19 @@ class Tests < Test::Unit::TestCase
   def test_created_resolve(r=mock)
     stubs(:created?).with(r).returns(true)
     assert_equal resolve(r), :created
+    r
   end
 
   def test_post_created(r=mock)
     stubs(:meth).with(r).returns(:post)
     assert_equal created?(r), true
+    r
   end
 
   def test_post_resolve(r=mock)
-    r = test_created_resolve(test_post_created(r))
+    test_created_resolve(test_post_created(r))
     assert_equal resolve(r), :created
+    r
   end
 
   def test_post_resolve2
